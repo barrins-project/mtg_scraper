143372ea1b93a6d0d378ff441fc187fa59a05059
Spigushe <martin.cuchet@gmail.com>
2025-05-19 18:51:12 +0200

    Add script to scrape tournaments without decks and remove empty deck files

---
+++ scraper/scripts/mtgo_empy_decks.py
@@ -0,0 +1,62 @@
+from scraper.utils import mtgo_utils
+from scraper.services.mtgo import consumer
+import json
+from queue import Queue
+from threading import Lock, Thread
+from scraper.utils import driver_utils
+from typing import DefaultDict, List
+from collections import defaultdict
+from pathlib import Path
+
+
+def get_tournaments_without_decks(queue: Queue, lock: Lock) -> None:
+    empty_files: List[Path] = []
+
+    for path in mtgo_utils.BASE_PATH.rglob("*.json"):
+        try:
+            with open(path, encoding="utf-8") as f:
+                data = json.load(f)
+                if data.get("decks") == []:
+                    empty_files.append(path)
+                    with lock:
+                        queue.put(data.get("tournament").get("url"))
+        except Exception as e:
+            print(f"Erreur lors de la lecture de {path}: {e}")
+
+    print(f"{len(empty_files)} fichier(s) trouvé(s) avec 'decks': []")
+
+    for file in empty_files:
+        file.unlink()
+
+
+def scrape_tournaments_without_decks(
+    num_threads: int = 4,
+):
+    task_queue = Queue()
+    lock = Lock()
+    drivers = [driver_utils.init_driver() for _ in range(num_threads)]
+    retries: DefaultDict[str, int] = defaultdict(int)
+
+    producer_thread = Thread(
+        target=get_tournaments_without_decks,
+        args=(task_queue, lock),
+    )
+
+    consumer_threads = [
+        Thread(
+            target=consumer,
+            args=(drivers[i], task_queue, lock, i + 1, retries),
+        )
+        for i in range(num_threads)
+    ]
+
+    producer_thread.start()
+    producer_thread.join()
+    for t in consumer_threads:
+        t.start()
+    for t in consumer_threads:
+        t.join()
+
+
+if __name__ == "__main__":
+    scrape_tournaments_without_decks()
